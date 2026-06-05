29513accf967b30f5e3e58146822471b7553e5cc
Soundarya <soundaryaekbote16@gmail.com>
2026-06-06 01:58:18 +0530

    Main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
